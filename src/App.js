f75f882925f37a6737e796a19809f2a14a04c80c
febanda01 <febanda91@gmail.com>
2019-07-09 23:19:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom'
+
+import Router from './Router'
+
+
+const Navigation = (props) => <nav>
+  <ul>
+    <li><NavLink to='/'>Home</NavLink></li>
+    <li><NavLink to='/cart'>Cart</NavLink></li>
+  </ul>
+</nav>
+
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/cartpage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function CartPage(props){
+    return(
+        <div>
+            <h2>MyCart</h2>
+        </div>
+    )
+}
